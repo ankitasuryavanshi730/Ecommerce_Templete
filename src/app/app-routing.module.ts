f61275753f54f75b3c35e00c62fa096d4d6093c0
Ankita Suryavanshi <ankitasuryavanshi730@gmail.com>
2023-02-08 17:11:20 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenComponent } from './components/men/men.component';
+import { WomenComponent } from './components/women/women.component';
+import { CheckoutComponent } from './mens/checkout/checkout.component';
+import { OrderCompleteComponent } from './mens/order-complete/order-complete.component';
+import { ProductDetailsComponent } from './mens/product-details/product-details.component';
+import { WishlistComponent } from './mens/wishlist/wishlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"home",component:HomeComponent},
+  {path:"about",component:AboutComponent},
+  {path:"men",component:MenComponent},
+  {path:"women",component:WomenComponent},
+  {path:"contact",component:ContactComponent},
+  {path:"cart",component:CartComponent},
+  {path:"checkout",component:CheckoutComponent},
+  {path:"order-complete",component:OrderCompleteComponent},
+  {path:"product-details",component:ProductDetailsComponent},
+  {path:"wishlist",component:WishlistComponent},
+  {path:"**",redirectTo:'/'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { MenComponent } from './components/men/men.component';
+import { WomenComponent } from './components/women/women.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductDetailsComponent } from './mens/product-details/product-details.component';
+import { CheckoutComponent } from './mens/checkout/checkout.component';
+import { WishlistComponent } from './mens/wishlist/wishlist.component';
+import { OrderCompleteComponent } from './mens/order-complete/order-complete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    MenComponent,
+    WomenComponent,
+    ContactComponent,
+    CartComponent,
+    ProductDetailsComponent,
+    CheckoutComponent,
+    WishlistComponent,
+    OrderCompleteComponent,
+
   ],
   imports: [
     BrowserModule,
